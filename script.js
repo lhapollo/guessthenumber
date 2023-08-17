5a11b5fd0817ac062b9b325b4608f54e6c5578d6
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2023-08-17 12:02:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const num = Math.floor(Math.random() * 100) + 1;
+var numGuesses = 1;
 // var out = document.getElementById('number');
 // out.textContent = num;
 
 function checkGuess() {
   const guess = parseInt(document.getElementById("guess").value);
   const message = document.getElementById("message");
+  
 
   if (guess === num) {
-    message.style.color = "green";
-    message.textContent = "Congrats!";
+    message.style.color = "rgb(59, 217, 89)";
+    message.textContent = "Congrats! That was the Number! Guessed in " + numGuesses + (numGuesses == 1? " try!": " tries!");
   } else if (guess < num) {
-    message.style.color = "red";
+    message.style.color = "rgb(217, 72, 44)";
     message.textContent = "Too low. Try again.";
+    numGuesses++;
   } else {
-    message.style.color = "red";
+    message.style.color = "rgb(217, 72, 44)";
     message.textContent = "Too high. Try again.";
+    numGuesses++;
   }
 }
